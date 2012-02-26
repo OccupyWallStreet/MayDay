08d81590b8ceb1887a291682102d267d6ac90459
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-02-26 16:18:35 -0500

    added css and js to application/config/LayoutPublic.php, which is how you include css/html docs

---
+++ application/config/LayoutPublic.php
@@ -0,0 +1,12 @@
+<?
+	$config['resources']['css'] = array(
+		'lattice/lattice/resources/thirdparty/960Grid/reset.css',
+		'lattice/lattice/resources/thirdparty/960Grid/grid12Fluid.css',
+		'application/resources/css/styles.css'
+	);
+	$config['resources']['js'] = array(
+		'lattice/lattice/resources/thirdparty/mootools/mootools.js',
+		'application/resources/js/imgSizer.js'
+	);
+	return $config;
+?>
