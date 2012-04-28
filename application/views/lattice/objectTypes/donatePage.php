4b896a44e319cfc09fbd3291243ee59a2f961d85
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-04-28 11:50:50 -0400

    added custom cms view for donate page

---
+++ application/views/lattice/objectTypes/donatePage.php
@@ -0,0 +1,13 @@
+<div class="clearFix">
+	<div class="grid_6 alpha">
+		<?=$text_intro;?>
+		<?=$text_blurb;?>
+		<?=$text_pageMeta;?>
+		<?=$contentBlocks;?>
+	</div>
+	<div class="grid_4 omega">
+		<?=$text_tweetsIntro;?>
+		<?=$sidebarContentBlocks;?>
+		<?=$donateResources;?>
+	</div>
+</div>
